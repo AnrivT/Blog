b9426fa6d1c0a11b2c026d88351892565fa74546
AnrivT <anri.vto@gmail.com>
2024-11-27 15:23:55 +0200

    Added usercontroller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//use model
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+}
